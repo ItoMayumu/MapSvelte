6da940f33930fa6cae2a4101b2528c6401b5245f
mayumu <mayumu329@icloud.com>
2024-08-02 21:19:05 +0900

    Map 表示対応

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import './app.css'
 import App from './App.svelte'
 
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+
 const app = new App({
   target: document.getElementById('app')!,
+  props: { ready: false as never }
 })
 
+window.initMap = function ready() {
+  app.$set({ ready: true });
+}
 export default app
